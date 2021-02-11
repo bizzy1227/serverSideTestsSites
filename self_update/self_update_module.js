9158c21657d5be1a31012107e205d03f7082d07b
Jenia22f <ye.altukhov@gmail.com>
2021-02-11 16:36:38 +0200

    test 1

--- self_update/self_update_module.js
+++ self_update/self_update_module.js
@@ -15,7 +15,7 @@ const selfUpdate  = async function(inputURL, withLogs) {
         ]
     });
     // '--headless'
-    driver = await new Builder().forBrowser('chrome')
+    driver = await new Builder().forBrowser('/usr/bin/google-chrome')
     .setChromeOptions(new chrome.Options().addArguments(['--ignore-certificate-errors', '--ignore-ssl-errors', '--headless']))
     .build();
 
