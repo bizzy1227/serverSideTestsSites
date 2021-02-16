9065c01eebe495b70d41fa610a3f41e69f26f2e7
Jenia22f <ye.altukhov@gmail.com>
2021-02-16 17:01:25 +0200

    added try catch in bot

--- telegram_bot/bot.js
+++ telegram_bot/bot.js
@@ -22,43 +22,36 @@ const BOT_TOKEN = '1668307994:AAGzNQ1wG8A2_0q2khMUhkPk7NzBi3wEvFE';
         })
 
         console.log(domains);
-		
+
 		let lastresult = '';
-        let result = await request.post('/site', { "sites": domains }).then(res => {
-			console.log('11111111', res.data);
-			for (let site in res.data) {
-				console.log('site', site);
-				lastresult += `>>>>>>>SITE: ${site}<<<<<<<\n`;
-				for (let page in res.data[site].webErrors) {
-					console.log('res.data[site].webErrors', res.data[site].webErrors);
-					lastresult += `PAGE: ${page}\n`;
-					if (res.data[site].webErrors[page].length === 0) {
-						lastresult += `error: no errors 👍`;
-						continue;
-					}
-					console.log('test' , res.data[site].webErrors[page]);
-					
-					res.data[site].webErrors[page].forEach(error => {
-						lastresult += `error: ${error.message}\n`;
-					})
-					lastresult += '\n';
-				}
-				lastresult += '---------------------------\n';
-			}
-            ctx.reply(lastresult)
-        });
-
-        
-
-        
 		
-
-		// const responseMessage = checkFullResult.map(r => {
-		// 	if (r.canSubmit && r.crm) return `${r.domain} 👍`
-		// 	else return `${r.domain} 😢 ${r.message.join('\n')}`
-		// })
-
-		// ctx.reply(res.data)
+		try {
+			let result = await request.post('/site', { "sites": domains }).then(res => {
+				console.log('11111111', res.data);
+				for (let site in res.data) {
+					console.log('site', site);
+					lastresult += `>>>>>>>SITE: ${site}<<<<<<<\n`;
+					for (let page in res.data[site].webErrors) {
+						console.log('res.data[site].webErrors', res.data[site].webErrors);
+						lastresult += `PAGE: ${page}\n`;
+						if (res.data[site].webErrors[page].length === 0) {
+							lastresult += `error: no errors 👍\n`;
+							continue;
+						}
+						console.log('test' , res.data[site].webErrors[page]);
+						
+						res.data[site].webErrors[page].forEach(error => {
+							lastresult += `error: ${error.message}\n`;
+						})
+						lastresult += '\n';
+					}
+					lastresult += '---------------------------\n';
+				}
+				ctx.reply(lastresult)
+			});
+		} catch (error) {
+			ctx.reply(error.message)
+		}
 	})
 
 	bot.launch()
