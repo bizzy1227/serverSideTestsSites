6d2ae100353e673d8fe649725cdeb0fda4873f8c
Jenia22f <ye.altukhov@gmail.com>
2021-02-16 15:25:49 +0200

    added push new sites for auto test

--- server_side_web_errors/weberrorsApp.js
+++ server_side_web_errors/weberrorsApp.js
@@ -3,6 +3,8 @@ const { request } = require('express');
 const app = express();
 const port = 9000;
 const mainProcc = require('../index');
+const fs = require("fs");
+const { url } = require('inspector');
 
 
 var server = app.listen(port, (err) => {
@@ -24,6 +26,8 @@ app.get('/', (request, response) => {
 })
 
 app.post('/site', async (request, response) => {
+
+    addToSiteQuery(request.body.sites);
     console.log('req body', request.body);
     try {
         let res = await mainProcc.runServerWebErrors(request.body.sites);
@@ -32,7 +36,37 @@ app.post('/site', async (request, response) => {
         console.log(error);
         response.send(error.message);
     }
-
 })
+
+const addToSiteQuery = async function(newSites) {
+    let oldUrls;
+    let newUrls;
+    let strSite = fs.readFileSync('../inputAutoWebErrors.txt', 'utf8');
+
+    let oldSites = strSite.split(/\r?\n/);
+    oldUrls = oldSites.map(function(site) {
+        if (!site.startsWith('http')) site = 'https://' + site;
+        let data = new URL(site);
+        return data.origin;
+    });
+
+    newUrls = newSites.map(function(site) {
+        if (!site.startsWith('http')) site = 'https://' + site;
+        let data = new URL(site);
+        return data.origin;
+    });
+
+    console.log('oldUrls', oldUrls)
+    console.log('newUrls', newUrls)
+
+    for (let elem of newUrls) {
+        if (oldUrls.includes(elem)) continue;
+        else {
+            fs.appendFileSync('../inputAutoWebErrors.txt', '\n' + elem, function (err) {
+                if (err) throw err;
+            });
+        }
+    }
+}
 
 
