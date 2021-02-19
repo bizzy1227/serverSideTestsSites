56c51a6ff6744b24b04267ce02e1f4495fa03c2c
Jenia22f <ye.altukhov@gmail.com>
2021-02-19 10:36:37 +0200

    change deovces to old

--- devices.js
+++ devices.js
@@ -10,21 +10,21 @@ const DEVICES = [
         'browserstack.key' : 'Y5QWsrsNx9pjNdHkZnKN'
     },
     {
-        'device' : 'Samsung Galaxy Note 8',
-        'realMobile' : 'true',
-        'os_version' : '7.1',
-        'browserName' : 'Android',
+        'os_version' : 'Lion',
+        'resolution' : '1920x1080',
+        'browserName' : 'Safari',
+        'browser_version' : '6.0',
+        'os' : 'OS X',
         'name': 'BStack-[NodeJS] Sample Test', // test name
         'build': 'BStack Build Number 1', // CI/CD job or build name
         'browserstack.user' : 'yaroslavsolovev1',
         'browserstack.key' : 'Y5QWsrsNx9pjNdHkZnKN'
     },
     {
-        'os_version' : '10',
-        'resolution' : '1920x1080',
-        'browserName' : 'Chrome',
-        'browser_version' : 'latest',
-        'os' : 'Windows',
+        'device' : 'Samsung Galaxy Tab 4',
+        'realMobile' : 'true',
+        'os_version' : '4.4',
+        'browserName' : 'Android',
         'name': 'BStack-[NodeJS] Sample Test', // test name
         'build': 'BStack Build Number 1', // CI/CD job or build name
         'browserstack.user' : 'yaroslavsolovev1',
