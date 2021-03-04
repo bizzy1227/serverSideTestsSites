90d3d82b08d2c2cd75c5bc852d31d9341fecedc7
Jenia22f <ye.altukhov@gmail.com>
2021-03-04 15:48:34 +0200

    added work with CRM returned data

--- server_side/app.js
+++ server_side/app.js
@@ -1,9 +1,11 @@
+const axios = require('axios');
 const express = require('express');
 const { request } = require('express');
 const app = express();
 const port = 8080;
 const mainProcc = require('../index');
 
+let testInProcess = false;
 
 var server = app.listen(port, (err) => {
     if (err) {
@@ -30,13 +32,44 @@ app.get('/', (request, response) => {
 app.post('/site', async (request, response) => {
     console.log('req body', request.body);
     try {
-        let res = await mainProcc.runServer(request.body.sites, 'sendFormErrors', request.body.typeSites);
-        response.send(res);
+
+        if (!testInProcess) {
+            if (request.body.callback) {
+                testInProcess = true;
+                response.send('Site(s) in processing'); 
+                let res = await mainProcc.runServer(request.body.sites, 'sendFormErrors', request.body.typeSites);
+                await setResultToCRM(res, request.body.callback);
+                testInProcess = false;
+            }
+            if (!request.body.callback) {
+                testInProcess = true;
+                let res = await mainProcc.runServer(request.body.sites, 'sendFormErrors', request.body.typeSites);
+                response.send(res);
+                testInProcess = false;
+            }
+        }
+        else {
+            response.send('The tests are currently running');
+        }
+
     } catch (error) {
+        testInProcess = false;
         console.log(error);
         response.send(error.message);
     }
 
 })
+
+async function setResultToCRM(res, callback) {
+
+    const requestSettings = axios.create({
+        baseURL: callback,
+    });
+    
+    await requestSettings.post('/', res).then(res => {
+        console.log('Result returned to CRM');
+    });
+
+}
 
 
