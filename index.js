d8020a5e6df821d7e50315a649404ef248c695ef
bizzy1227 <archstart1227@gmail.com>
2021-03-19 11:52:05 +0200

    changed default property neogara result to null

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ const runServer = async function(sites, typeRun, typeSites) {
             virusTotal: virusTotal,
             checkAvailability: checkAvailability,
             consoleErrors: consoleErrors,
-            neogaraResults: true
+            neogaraResults: null
         }
 
     }
