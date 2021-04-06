e4198667e833d0de3733452e5e1f4fa892f76bc3
bizzy1227 <archstart1227@gmail.com>
2021-04-06 13:54:17 +0300

    deleted comments

--- auto_check_availability/autoCheckAvailability.js
+++ auto_check_availability/autoCheckAvailability.js
@@ -45,34 +45,26 @@ async function buitySites(sites) {
 async function getProxyCheckAvailability(site) {
     console.log('in getProxyCheckAvailability');
     let result = [];
-        console.log('prik 1');
         
         for (let proxyItem of proxys) {
             result = [];
-            console.log('prik 2');
             const axiosDefaultConfig = {
                 proxy: proxyItem.settings
             };
-            console.log('prik 3');
 
             const axiosFixed = require ('axios-https-proxy-fix').create(axiosDefaultConfig);
-            console.log('prik 4');
             
             await axiosFixed.get(`${site}`)
             .then(function (response) {
-                console.log('prik 5');
                 // let r = JSON.stringify(response.data);
                 // console.log(`${site}`, response.status);
                 result.push({ country: proxyItem.country, status: response.status, contentLength: JSON.stringify(response.data).length });
             })
             .catch(function (error) {
-                console.log('prik 6');
                 console.log('error 1', 1);
                 result.push({ country: proxyItem.country, status: -1, contentLength: error.message.length });
             });
-            console.log('prik 7');
         }
-    console.log('prik 8');
     return result;
 }
 
@@ -127,12 +119,12 @@ async function main() {
     });
 
     let buityCrsDomains = await buitySites(crsDomains);
-    console.log('crsDomains buity', crsDomains);
+    // console.log('crsDomains buity', crsDomains);
     
     let proxyCheckResult;
 
     proxyCheckResult = await asyncCallToResult(buityCrsDomains);
-    console.log('proxyCheckResult', proxyCheckResult);
+    // console.log('proxyCheckResult', proxyCheckResult);
 
     for (let [index, domain] of crsDomains.entries()) {
         // console.log('test_3', index, domain);
