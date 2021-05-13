82c5e95c8e72b42929c94c5c98da2e818a55d5e4
bizzy1227 <archstart1227@gmail.com>
2021-05-13 22:00:52 +0300

    added handle error if set inccorect domain

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const runServer = async function(sites, typeRun, typeSites) {
             nodeUrl = new URL(inputURL);
         } catch (error) {
             mainRespone[inputURL] = {
-                testResult: false
+                testResult: 'Incorrect domain'
             };
             continue;
         }
